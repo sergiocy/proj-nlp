d0f344b5a671fa9aabaadcfc2b4cbd229d8411a3
sergiocy <sergiocordoba82@gmail.com>
2019-03-10 19:12:29 +0100

    implementing inputs argument treatment: config

--- app/main.py
+++ app/main.py
@@ -1,16 +1,12 @@
 
 import argparse
-
+import logging
 #import os
 #import traceback
-#import logging
 #import time
-
-import nltk
 
 from controller.selector import *
 from controller.InputArguments import InputArguments
-
 
 
 PATH_CONFIG = '../config/config.ini'
@@ -23,6 +19,7 @@ if __name__ == "__main__":
     obj_config = InputArguments(PATH_CONFIG, type_config_json = False)
     param_config = obj_config.get_input_arguments()
     print(param_config)
+    logging.info(param_config)
     
     parser = argparse.ArgumentParser()
     #parser.add_argument('--process', type=str, nargs='+', default=param_config.get('process'))
