897179f5b820e56615e5c360e7b9cc8a163e9bc3
sergiocy <sergiocordoba82@gmail.com>
2019-05-12 09:17:13 +0200

    testing bert

--- app/service/main_plan1.py
+++ app/service/main_plan1.py
@@ -3,22 +3,20 @@
 # -*- coding: utf-8 -*-
 
 #import logging
-
 import numpy as np
 import math
 import pandas as pd
-#import nltk
+from nltk.tokenize import word_tokenize
 #from gensim.models import Word2Vec
 import gensim
-
 #import tensorflow as tf
 #import tensorflow_hub as hub
-
-#from nltk.corpus import brown, movie_reviews, treebank
-
 from bert_embedding import BertEmbedding
 
-#from numpy.core import *
+#### imports personal modules
+from input_text.input_processing import clean_text
+
+
 
 
 
@@ -64,16 +62,53 @@ if __name__ == '__main__':
     PATH_INPUT_DATA = '../../config/input/wordsim353/combined.csv'
     PATH_INPUT_DATA_DEF = '../../config/input/wordsim353/combined-definitions.csv'
 
+
     ##################################################
     #### READING FILE INPUT
-    #data = pd.read_csv(PATH_INPUT_DATA)
+    data_sim = pd.read_csv(PATH_INPUT_DATA, sep=',', encoding='utf-8')
+    data_sim.columns = ['w1', 'w2', 'sim']
     #### ...we select a subset to develope...
-    #data = data.loc[0:10]
-    #print(data)
+    data_sim = data_sim.loc[0:10]
+    print(data_sim)
 
     data_def = pd.read_csv(PATH_INPUT_DATA_DEF, sep=';', encoding='utf-8')
+    data_def.columns = ['w1', 'def']
+    #### ...we select a subset to develope...
     data_def = data_def.loc[0:10]
     print(data_def)
+
+
+    ##################################################
+    #### PREPROCESSING DEFINITIONS
+    data_def = clean_text(data_def, 'def')
+    print(data_def)
+
+    set_words = []
+    set_words = data_def['def'].apply(lambda phrase: set_words.extend(list(phrase)))
+    print(set_words)
+
+
+
+
+
+    ############################################
+    #### BERT model
+    print('######### BERT ############')
+    #sen1 = data_def['definition'][0]
+    #print(sen1)
+#
+#
+    #bert_embedding = BertEmbedding()
+    ##result = bert_embedding(['something', 'to', 'encode'])
+    ##result = bert_embedding(['something to encode'])
+    #result = bert_embedding([sen1])
+    #print(type(sen1))
+#
+    #first = result[0]
+#
+    ##print(first[1])
+    #print(first[1][1].shape)
+
 
     ##################################################
     #### ...load Google Word2Vec model...
@@ -88,24 +123,8 @@ if __name__ == '__main__':
     #wemb_sex = model.wv['sex']
     #print(wemb_love)
 
-    ############################################
-    #### BERT model
-    print('##########################')
-    sen1 = data_def['definition'][0]
-    print(sen1)
 
 
-
-    bert_embedding = BertEmbedding()
-    #result = bert_embedding(['something', 'to', 'encode'])
-    #result = bert_embedding(['something to encode'])
-    result = bert_embedding([sen1])
-    print(type(sen1))
-
-    first = result[0]
-
-    #print(first[1])
-    print(first[1][1].shape)
 
 
     
