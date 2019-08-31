8ab60e80525d4e6fa4b70a1d3c7f8d3258af3321
sergiocy <sergiocordoba82@gmail.com>
2019-08-31 23:45:18 +0200

    reviewing code structure and functionalities

--- app/main_plan1.py
+++ app/main_plan1.py
@@ -2,7 +2,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#import logging
+import logging
+import os
 #import numpy as np
 #import math
 import pandas as pd
@@ -16,12 +17,14 @@ import gensim
 from bert_embedding import BertEmbedding
 
 #### imports personal modules
-from service.input_text.input_processing import clean_text, get_set_of_tokens
-from service.vectorizer.bert_vectorizer import get_bert_embedding_of_one_token
-from service.vectorizer.vector_computing import compute_mean_vector
+from service.util.utils import create_logger
+from service.text.input_text.input_processing import clean_text, get_set_of_tokens
+from service.vectorization.bert_vectorizer import get_bert_embedding_of_one_token
+from service.computing.vector_computing import compute_mean_vector
 
 
 
+PATH_LOG_FILE = '../log/log.log'
 PATH_W2V_MODEL = '../config/model/GoogleNews-vectors-negative300.bin'
 PATH_INPUT_DATA = '../config/input/wordsim353/combined.csv'
 PATH_INPUT_DATA_DEF = '../config/input/wordsim353/combined-definitions.csv'
@@ -29,7 +32,13 @@ PATH_OUTPUT_BERT_DATA_DEF = '../config/output/combined-definitions'
 
 
 
-def read_inputs():
+
+
+def read_inputs(new_colnames
+                , logger = None
+                , file_input = PATH_INPUT_DATA_DEF
+                , sep = ';'
+                , encoding = 'utf-8'):
     ##################################################
     #### READING FILE INPUT
     #data_sim = pd.read_csv(PATH_INPUT_DATA, sep=',', encoding='utf-8')
@@ -38,11 +47,10 @@ def read_inputs():
     #data_sim = data_sim.loc[0:10]
     #print(data_sim)
 
-    data_def = pd.read_csv(PATH_INPUT_DATA_DEF, sep=';', encoding='utf-8')
-    data_def.columns = ['w1', 'def']
+    data_def = pd.read_csv(file_input, sep=sep, encoding=encoding)
+    data_def.columns = new_colnames
     #### ...we select a subset to develope...
     #data_def = data_def.loc[0:2]
-    print(data_def)
 
     return data_def
 
@@ -88,20 +96,29 @@ def compute_bert(data_def):
 
 
 if __name__ == '__main__':
+    os.remove(PATH_LOG_FILE)
+    logger = create_logger(PATH_LOG_FILE)
+    logger.info(' - starting execution')
 
     #### READING FILES
-    #data_def = read_inputs()
+    logger.info(' - read file {0}'.format(PATH_INPUT_DATA_DEF))
+    data_def = read_inputs( logger = logger
+                            , file_input = PATH_INPUT_DATA_DEF
+                            , new_colnames = ['w1', 'def'])
+    logger.info(' - dataframe loaded; first rows...')
+    logger.info('\n{0}'.format(data_def.loc[0:2]))
+
     #data_def = process_inputs(data_def)
     #### COMPUTING BERT-VECTORS
     #data_def = compute_bert(data_def)
     #data_def.to_pickle(PATH_OUTPUT_BERT_DATA_DEF)
 
 
-    data_def = pd.read_pickle(PATH_OUTPUT_BERT_DATA_DEF)
-    data_def = data_def.loc[0:2]
+    #data_def = pd.read_pickle(PATH_OUTPUT_BERT_DATA_DEF)
+    #data_def = data_def.loc[0:2]
 
-    print(data_def)
-    print(data_def.shape)
+    #print(data_def)
+    #print(data_def.shape)
 
 
     ##################################################
