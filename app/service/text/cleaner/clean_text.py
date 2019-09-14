9e9bbff6c457660b2b47c9a284e61112ffd85a0a
sergiocy <sergiocordoba82@gmail.com>
2019-09-14 09:55:23 +0200

    working on text cleaner

--- app/service/text/cleaner/clean_text.py
+++ app/service/text/cleaner/clean_text.py
@@ -6,9 +6,10 @@ from nltk.tokenize import word_tokenize
 
 
 
+#### CLEAN ENGLISH TOKEN
 def clean_english_token(token_in
-                , lcase=True
-                , logger=None):
+                        , lcase=True
+                        , logger=None):
     
     token = token_in
     
@@ -32,41 +33,41 @@ def clean_english_token(token_in
 
 
 
+
 #### CLEAN PHRASE
 #### NOTE: we must do it for works with only one token
-def clean_phrase(phrase_in
+def clean_phrase(str_phrase_in
                 , language = 'en'
                 , lcase=True
                 , tokenized=False
                 , logger=None):
    
-    phrase = phrase_in
+    phrase = str_phrase_in
 
     try:
-        print('-----------------')
-        print(phrase)
         if language == 'en':
             phrase = word_tokenize(phrase)
             phrase = [clean_english_token(token) for token in phrase]
-        print(phrase)
 
         if not tokenized:
             phrase = ' '.join(phrase)
 
-        print(phrase)
-        print('-----------------')
+        if logger is not None:
+            logger.info('phrase \'{0}\' cleaned as \'{1}\''.format(str_phrase_in, phrase))
         
     except Exception:
         if logger is not None:
-            logger.exception("ERROR cleaning phrase \'{0}\'".format(phrase_in))
+            logger.exception("ERROR cleaning phrase \'{0}\'".format(str_phrase_in))
         raise Exception
             
     return phrase
 
 
+
+
 if __name__ == '__main__':
-    phrase = 'To Like something'
-    phrase = 'To Like something'
+    phrase1 = 'To Like something'
+    phrase2 = 'To Like something'
 
     clean_phrase(phrase)
 
