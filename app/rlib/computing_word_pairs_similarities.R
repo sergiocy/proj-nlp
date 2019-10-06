269b7c6c9d37c5c6d558c1c456417480678f7d55
sergiocy <sergiocordoba82@gmail.com>
2019-10-06 22:49:16 +0200

    computing similarities matrix

--- app/rlib/computing_word_pairs_similarities.R
+++ app/rlib/computing_word_pairs_similarities.R
@@ -9,20 +9,16 @@ library("data.table")
 FILE_PY_READ_PICKLE <- "C:/sc-sync/projects/proj-nlp/app/service/util/read_pickle.py"
 FILE_PXL_TO_READ <- "C:/sc-sync/projects/proj-nlp/data/output/combined-definitions-complete"
 
-
-
-
 #### read dataframe from .pkl file
 source_python(FILE_PY_READ_PICKLE)
 pickle_data <- read_pickle_file(FILE_PXL_TO_READ)
 
-
 data <- setDT(pickle_data)
 rm(pickle_data)
 
-dim(data)
-str(data)
-colnames(data)
+#dim(data)
+#str(data)
+#colnames(data)
 
 
 
@@ -57,22 +53,51 @@ compute_similarity_cosin_between_vectors <- function(v1, v2, str1 = 'word1', str
         }
     )
     
-    return(sim)
+    return(as.numeric(sim))
 }
 
-# compute_similarity_cosin_between_vectors(c(1,2,3,5), c(1,2,3,4))
+# compute_similarity_cosin_between_vectors(c(1,0), c(0,1))
+
+
+
+####
+#### ...we select word1 and definitions (from composition of definition words) for it
+data_sim_w1 <- subset(data, select = c(w1, w1_vectorized, def1_vector_sum
+                                       #, def1_vector_avg
+                                       ))
+#length(data_sim_w1[1, , ]$def1_vector_sum)
+colnames(data_sim_w1) <- c("w", "vec_w", "vec_composed")
 
 
 
 
-data_sim_w1 <- subset(data, select = c(w1, w2, def1_vector_sum, def1_vector_avg, def2_vector_sum, def2_vector_avg))
+####
+#### define an empty datatable
+matrix_sim <- data.table( word = data_sim_w1$w )
+#sapply(1:length(matrix_sim$word), function(i_w) { matrix_sim[ , paste0("comp_", matrix_sim$word[i_w]) := NA , ]  } )
 
-#### data to dev
-iter_on_words <- 1
-iter_definitions <-1 #### definition selection between 1 - 345
+sapply(1:length(data_sim_w1$w)
+       , function(iter_on_words) {
+                                    word = data_sim_w1$w[iter_on_words]
+                                    # vector associated to one only word
+                                    vec_word <- unlist(data_sim_w1[ iter_on_words, .(vec_w), ][[1]]) 
+                                    
+                                    # ...we compute similarity between a word an a set of composition-vectors
+                                    vec_word_vs_composed <- sapply(1:length(data_sim_w1$vec_composed)
+                                                                   , function(i_def){
+                                                                       vec_def <- unlist(data_sim_w1[ i_def, .(vec_composed), ][["vec_composed"]])
+                                                                       return (compute_similarity_cosin_between_vectors(vec_word, vec_def))
+                                                                   })
+                                    
+                                    matrix_sim <- rbind(matrix_sim, c(word, vec_word_vs_composed))
+                                    
+                                    #print(paste0(iter_on_words, " - ", word, " - ", vec_word[1]))
+                                   })
 
-w_vec <- data_sim_w1[ 1, w1_vectorized, ][[1]][[iter_on_words]] # vector associated to one only word
-    w_def <- data_sim_w1[ , def1_vectorized, ][[iter_definitions]] # set of vectors associated to each word in definition
+    
+    
+
+
     
 
 
